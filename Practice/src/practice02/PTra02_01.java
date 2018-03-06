6e26f266c277eec42bbbb949593830ec8c90081c
MitsukoTakeuchi <e5mary49@gmail.com>
2018-03-06 16:42:14 +0900

    2-1

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -1,4 +1,5 @@
 package practice02;
+
 /*
  * PTra02_01.java
  *   作成	LIKEIT	2017
@@ -11,12 +12,17 @@ public class PTra02_01 {
 
 		// ★ 20を5で割った値を出力してください
 
+		System.out.println(20/5);
+
 
 		// ★ 126に13を掛けた値を出力してください
+
+		System.out.println(126*13);
 
 
 		// ★ 97を6で割ったときの余りを出力してください
 
+		System.out.println(97%6);
 
 	}
 }
