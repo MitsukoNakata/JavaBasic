049ea72f6b9835cc146112a6c1b45bc8d62c4c5e
MitsukoTakeuchi <e5mary49@gmail.com>
2018-03-20 16:39:06 +0900

    10-1

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -26,11 +26,21 @@ public class PTra10_01 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
+		User us = new User();
+
 
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
+		us.userId = 6361;
+		us.userNm = "竹内光子";
+		us.mail = "e5mary49@gmail.com";
+		us.password = "12345678";
+
 
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		System.out.println("userIdは"+us.userId);
+		System.out.println("userNmは"+us.userNm);
+		System.out.println("mailは"+us.mail);
+		System.out.println("passwordは"+us.password);
 	}
 }
---
+++ Practice/src/practice10/User.java
@@ -0,0 +1,12 @@
+package practice10;
+
+public class User {
+	/**属性**/
+
+	public int userId;   //접근수식자 필드 자료형 필드명 순으로 정의해야합니다
+	public String userNm; //아무것도 붙이지 않는다면 같은 패키지 안에서만 불러올수 있습니다.
+	public String mail;   //
+	public String password;
+
+
+}
